83d4881db39412efff2645fa01f248ed2d8fcea5
Brian McClure <b.mcclure@jumediavision.de>
2014-09-08 10:46:09 +0200

    forgot to include templates/ in pypi dist

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name     = 'django-media-helper',
-    version  = '0.1.1',
+    version  = '0.1.2',
     packages = find_packages(),
     include_package_data=True,
     requires = ['python (>= 2.5)', 'django (>= 1.6)', 'Pillow (>= 2.1.0)'],
@@ -21,8 +21,8 @@ setup(
     long_description = open('README.markdown').read(), 
     author       = 'Brian McClure',
     author_email = 'brian.mcclr@gmail.com',
-    url          = 'https://bitbucket.org/brmcllr/django_media_helper',
-    download_url = 'https://bitbucket.org/brmcllr/django_media_helper.git',
+    url          = 'https://bitbucket.org/brmcllr/django-media-helper',
+    download_url = 'https://bitbucket.org/brmcllr/django-media-helper.git',
     license      = 'MIT License',
     keywords     = 'django, imaging, ajax',
     classifiers  = [
