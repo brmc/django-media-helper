704a8771d50b315f7b8eedb28a9e5d204ab36612
Brian McClre <brian.mcclr@gmail.com>
2015-02-14 17:30:20 +0100

    stupid setup.py.  Finally just hard-coded the long-description

--- setup.py
+++ setup.py
@@ -9,6 +9,19 @@
 
 from setuptools import setup, find_packages
 
+description = """# **django-media-helper** #
+
+When dealing with content from unacquainted sources(e.g., clients or designers)
+one often gets images with absurd dimensions and/or filesizes: A 3000px-wide
+play-button, a 10MB logo, etc.  Media-helper attempts to mitigate this problem
+by automating image-resizing, delivering the most appropriately sized image to
+the browser.
+
+It is also designed to be dropped into existing projects with minimal effort.
+It's still in the alpha stage, but if you're careful it might make your life a
+little bit easier while also speeding up your load times and reducing data
+transfer."""
+
 setup(
     name     = 'django-media-helper',
     version  = '0.3',
@@ -16,7 +29,7 @@ setup(
     include_package_data=True,
     requires = ['python (>= 2.7)', 'django (>= 1.6)', 'Pillow (>= 2.1.0)'],
     description  = 'An image resizing and management app for Django',
-    long_description = open('DESCRIPTION.markdown').read(),
+    long_description = description,
     author       = 'Brian McClure',
     author_email = 'brian.mcclr@gmail.com',
     url          = 'https://github.com/brmc/django-media-helper',
